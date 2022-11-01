dd50afee9d10601714dfdd390f035280065f8699
candy-Tong <563378816@qq.com>
2022-11-01 21:01:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-sxjhy4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-sxjhy4)
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+// import 'vue-router';
+
+const count = ref(0);
+setInterval(() => {
+  count.value++;
+}, 1000);
+</script>
+
+<template>
+  <h5>将 import 'vue-router'; 取消注释</h5>
+  <h5>页面会刷新，count 会被重置</h5>
+  <div>{{ count }}</div>
+</template>
